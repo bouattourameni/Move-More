2b55bd7be370f678b665c12cc88563bac4bb1158
bouattourameni <bouattour.ameni@gmail.com>
2015-04-27 21:54:23 +0200

    ameni

--- app/src/main/java/ai/project/pfa/movemore/Step/Vibration.java
+++ app/src/main/java/ai/project/pfa/movemore/Step/Vibration.java
@@ -46,7 +46,7 @@ public double getMax(double x, double y){
             else {
                 float f = Fuzzy.evaluate(x_axes,y_axes,z_axes);
                 if (f>0.4){
-                    ModeNormal.steps.setText(Integer.parseInt(String.valueOf(ModeNormal.steps.getText()))+1);
+                    ModeNormal.steps.setText(Integer.toString(Integer.parseInt(String.valueOf(ModeNormal.steps.getText()))+1));
                 }
 
                 nbre_msg = 0;
