27fbb52a07e3b69927f75f71c6c646e63e2e8d11
bouattourameni <bouattour.ameni@gmail.com>
2015-04-27 22:07:33 +0200

    ameni

--- app/src/main/java/ai/project/pfa/movemore/Step/ModeNormal.java
+++ app/src/main/java/ai/project/pfa/movemore/Step/ModeNormal.java
@@ -56,7 +56,6 @@ public class ModeNormal extends Activity {
         setContentView(R.layout.activity_mode_normal);
 
         steps=(TextView)findViewById(R.id.steps);
-        steps.setText("0");
         ins = getResources().openRawResource(getResources().
                 getIdentifier("raw/pedometrefcl", "raw", getPackageName()));
         Intent i=new Intent(this, Vibration.class);
