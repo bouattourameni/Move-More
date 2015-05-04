e7779a428fba3da815c34f3c7078a03c23458639
bouattourameni <bouattour.ameni@gmail.com>
2015-05-05 01:36:53 +0200

    ameni

--- app/src/main/java/ai/project/pfa/movemore/data/StepProvider.java
+++ app/src/main/java/ai/project/pfa/movemore/data/StepProvider.java
@@ -1,7 +1,41 @@
 package ai.project.pfa.movemore.data;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
 /**
  * Created by user on 05/05/2015.
  */
-public class StepProvider {
+public class StepProvider extends ContentProvider {
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
 }
