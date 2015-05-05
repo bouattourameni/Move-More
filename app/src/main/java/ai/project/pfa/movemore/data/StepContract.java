d5ceea468bd1a28fba11752f6ffa095630f44d28
bouattourameni <bouattour.ameni@gmail.com>
2015-05-05 23:10:10 +0200

    ameni

--- app/src/main/java/ai/project/pfa/movemore/data/StepContract.java
+++ app/src/main/java/ai/project/pfa/movemore/data/StepContract.java
@@ -13,8 +13,7 @@ public class StepContract {
 
     public static final String CONTENT_AUTHORITY = "ai.project.pfa.movemore";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
-    public static final String PATH_NORMAL = "normal";
-    public static final String PATH_SPORT = "sport";
+    public static final String PATH_STEP = "step";
 
     public static long normalizeDate(long startDate) {
         // normalize the start date to the beginning of the (UTC) day
@@ -27,18 +26,19 @@ public class StepContract {
     public static final class StepEntry implements BaseColumns {
         public static final String TABLE_NAME = "normal";
         public static final String COLUMN_DATE = "date";
+        public static final String COLUMN_TYPE = "type";
         public static final String COLUMN_NBRE_STEPS = "nbreSteps";
         public static final String COLUMN_TEMPS = "temps";
-        public static final String COLUMN_TYPE = "type";
+
 
         public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_NORMAL).build();
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_STEP).build();
 
         public static final String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_NORMAL;
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_STEP;
 
         public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_NORMAL;
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_STEP;
 
 
         public static Uri buildStepUri(long id) {
@@ -64,12 +64,15 @@ public class StepContract {
 //                    .appendQueryParameter(COLUMN_DATE, type)
 //                    .build();
 //        }
-        public static String getStepFromUri(Uri uri) {
-            return uri.getPathSegments().get(2);
+        public static String getNbreStepFromUri(Uri uri) {
+            return uri.getPathSegments().get(3);
         }
 
         public static long getDateFromUri(Uri uri) {
             return Long.parseLong(uri.getPathSegments().get(1));
+        }
+        public static String getTypeFromUri(Uri uri) {
+            return uri.getPathSegments().get(2);
         }
     }
 
--- app/src/main/java/ai/project/pfa/movemore/data/StepProvider.java
+++ app/src/main/java/ai/project/pfa/movemore/data/StepProvider.java
@@ -20,10 +20,70 @@ public class StepProvider extends ContentProvider {
     public static final int STEP_WITH_TYPE = 101;
     public static final int STEP_WITH_TYPE_AND_DATE = 102;
 
-
+    SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+    {
+        qb.setTables(StepContract.StepEntry.TABLE_NAME);
+    }
 
     private static UriMatcher buildUriMatcher() {
 
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority=StepContract.CONTENT_AUTHORITY;
+        // 2) Use the addURI function to match each of the types.  Use the constants from
+        // WeatherContract to help define the types to the UriMatcher.
+        matcher.addURI(authority, StepContract.PATH_STEP, STEP);
+        matcher.addURI(authority, StepContract.PATH_STEP + "/date/*", STEP_WITH_TYPE);
+        matcher.addURI(authority, StepContract.PATH_STEP + "/#/*", STEP_WITH_TYPE_AND_DATE);
+
+
+        return matcher;
+    }
+    private static final String sTypeAndDateSelection =
+            StepContract.StepEntry.TABLE_NAME+
+                    "." + StepContract.StepEntry.COLUMN_TYPE + " = ? AND " +
+                    StepContract.StepEntry.COLUMN_DATE + " >= ? ";
+    private static final String sLocationSettingSelection =
+            StepContract.StepEntry.TABLE_NAME+
+                    "." + StepContract.StepEntry.COLUMN_TYPE + " = ? ";
+
+    private Cursor getStep(Uri uri, String[] projection, String sortOrder) {
+     return qb.query(mOpenHelper.getReadableDatabase(),
+                projection,
+                null,
+                null,
+                null,
+                null,
+                sortOrder
+        );
+    }
+    private Cursor getStepByType(
+            Uri uri, String[] projection, String sortOrder) {
+
+        String type = StepContract.StepEntry.getTypeFromUri(uri);
+
+        return qb.query(mOpenHelper.getReadableDatabase(),
+                projection,
+                sTypeAndDateSelection,
+                new String[]{type},
+                null,
+                null,
+                sortOrder
+        );
+    }
+    private Cursor getStepByTypeAndDate(
+            Uri uri, String[] projection, String sortOrder) {
+
+        long date = StepContract.StepEntry.getDateFromUri(uri);
+        String type = StepContract.StepEntry.getTypeFromUri(uri);
+
+        return qb.query(mOpenHelper.getReadableDatabase(),
+                projection,
+                sTypeAndDateSelection,
+                new String[]{type, Long.toString(date)},
+                null,
+                null,
+                sortOrder
+        );
     }
     @Override
     public boolean onCreate() {
@@ -34,31 +94,32 @@ public class StepProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
-        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
-        qb.setTables(StepContract.StepEntry.TABLE_NAME);
+        Cursor retCursor;
+        switch (sUriMatcher.match(uri)) {
 
-        Cursor c = qb.query(mOpenHelper.getReadableDatabase(),
-                //colonnes
-                projection,
-                //where
-                selection,
-                //oderby
-                selectionArgs,
-                null, null, null);
-        c.setNotificationUri(getContext().getContentResolver(), uri);
+            case STEP:
+            {
+                retCursor = getStep(uri, projection, sortOrder);
+                break;
+            }
 
-        return c;
-//        Cursor retCursor;
-//        retCursor = mOpenHelper.getReadableDatabase().query(
-//                StepContract.NormalEntry.TABLE_NAME,
-//                projection,
-//                selection,
-//                selectionArgs,
-//                null,
-//                null,
-//                sortOrder
-//        );
-//        return  retCursor;
+            case STEP_WITH_TYPE: {
+                retCursor = getStepByType(uri, projection, sortOrder);
+                break;
+            }
+
+            case STEP_WITH_TYPE_AND_DATE: {
+                retCursor = getStepByTypeAndDate(uri, projection, sortOrder);
+
+                break;
+
+            }
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return retCursor;
     }
 
     @Override
